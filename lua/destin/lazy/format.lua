46a3f0eecf0e235a24b286b96468601726636b7e
Destin Lee <dleeinc@gmail.com>
2024-04-25 10:05:38 -0500

    Adding gopls

--- lua/destin/lazy/format.lua
+++ lua/destin/lazy/format.lua
@@ -7,6 +7,7 @@ return {
     conform.setup({
       formatters_by_ft = {
         lua = { "stylua" },
+        go = { "gopls", "goimports" },
         python = { "black" },
         bash = { { "shfmt" } },
         sh = { { "shfmt" } },
